49d2ca23058462e0e9a6832f874cc65846f3a5e1
nSakkriou <nathansakkriou@gmail.com>
2023-07-23 17:31:18 +0200

    Insertion donnée IRONMAN pour les ajouter dans le calendrier de manière un peu degeulasse, trouver autre chose

--- api.py
+++ api.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+
 import json, uvicorn
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 
-templates = Jinja2Templates(directory="./view")
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="./view/static"), name="static")
+templates = Jinja2Templates(directory="./view")
 
 with open("data.json", 'r', encoding="utf8") as f:
-    data = {"data" : json.loads(f.read())}
+    data = {"data" : f.read()}
 
 @app.get("/")
 def root(request: Request):
